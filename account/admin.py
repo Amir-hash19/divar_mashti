61b20f2e9deb0a760a7e1865db18b1ac48905bc6
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-04-13 13:22:13 +0330

    :bug:

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import UserAccount
 
-# Register your models here.
+       
+
+@admin.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ("fullname", "email", "date_created")
+    list_filter = ("is_superuser", "date_created")
+    search_fields = ("date_created", "fullname")
+  
+
+  
+    def get_ordering(self, request):
+        if request.user.is_superuser:
+            return ("fullname", "email")
+        return ("email", )
--- account/models.py
+++ account/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+
+class UserAccountManager(BaseUserManager):
+    
+    def create_user(self, email, fullname, password=None):
+        if not email:
+            raise ValueError("User must have an email")
+        else:
+            email = self.normalize_email(email)
+            user = self.model(email=email, fullname=fullname)
+            user.set_password(password)
+            user.save()
+            return user
+        
+
+    def create_superuser(self, email, fullname, password):
+        user = self.create_user(email=email, fullname=fullname, password=password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+    
+
+
+class UserAccount(AbstractBaseUser):
+    email = models.EmailField(max_length=120, unique=True)
+    fullname = models.CharField(max_length=150)
+    phone = PhoneNumberField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["fullname"])
+        ] 
+        
+        
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['fullname']
+    objects = UserAccountManager()
+
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
+    
+
+    def __str__(self):
+        return self.fullname
+
+
+
+
+
